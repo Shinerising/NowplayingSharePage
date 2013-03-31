44e7a0d1002bc46d4e3cb1f7b0e11cb61e27d705
Apollo Wayne <waynezhaoyang@gmail.com>
2013-03-31 17:30:04 +0800

    Revert "Main Page Background Image"
    This reverts commit bf09474017c04de230d1c71f5dcb75205162ff17.

--- backimg.php
+++
@@ -1,8 +0,0 @@
-<?php
-header('Content-Type: image/jpeg');
-$url = $_GET["url"];
-$str = 'convert ' . $url . ' 200x200 -blur 0x30 back.jpg';
-exec($str); 
-$im = imagecreatefromjpeg("back.jpg");
-imagejpeg($im,'',80);
-?>
